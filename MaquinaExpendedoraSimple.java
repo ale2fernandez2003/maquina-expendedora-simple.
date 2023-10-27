992f0501a408b19cabd18e132e1046448c824dd2
Alejandro <ale2fernandez2003@gmail.com>
2023-10-27 13:23:07 +0200

    metodo setter

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -49,6 +49,10 @@ public class MaquinaExpendedoraSimple {
     public int getTotalDineroAcumulado() {
         return totalDineroAcumulado;
     }
+    
+    public void setPrecioBillete(int precioDelBillete) {
+        precioBillete = precioDelBillete;
+    }
 
     /**
      * Simula la introduccion de dinero por parte del cliente actual
